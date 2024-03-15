853caccea3b8ff9103bde18766224104124f7a2e
Joac2363 <joachimstorbank@gmail.com>
2024-03-15 10:04:39 +0100

    Simple vector

---
+++ 3dSpaceEssentials.cs
@@ -0,0 +1,52 @@
+using System;
+
+namespace SpaceEssentials
+{
+	public class Vector()
+	{
+		public double x;
+		public double y;
+		public double z;
+
+		public Vector(double X, double Y, double Z) : this()
+		{
+			x = X; y = Y; z = Z;
+		}
+			
+		public double Len()
+		{
+			return Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2) + Math.Pow(z, 2));
+		}
+
+		public static Vector operator +(Vector a, Vector b)
+		{
+			double x = a.x + b.x;
+			double y = a.y + b.y;
+			double z = a.z + b.z;	
+			Vector vec = new Vector(x,y,z);
+			
+			return vec;
+		}
+
+		public static Vector operator -(Vector a, Vector b)
+		{
+			double x = a.x - b.x;
+			double y = a.y - b.y;
+			double z = a.z - b.z;
+			Vector vec = new Vector(x, y, z);
+
+			return vec;
+		}
+		public static Vector operator *(Vector a, double b)
+		{
+			double x = a.x * b;
+			double y = a.y * b;
+			double z = a.z * b;
+			Vector vec = new Vector(x, y, z);
+
+			return vec;
+		}
+
+	}
+
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SpaceEssentials;
+
+class Program
+{
+    static void Main()
+    {
+        Vector myVector = new Vector(13, 13, 13);
+        Vector other = new Vector(2, 2, 2);
+        
+        Console.WriteLine((other *4).x);
+    }
+}
